598808130ee86e02ee95b63a94c5b819bb6c405d
Gowtham Kumar K <gowthamkumar.kg@gmail.com>
2025-09-06 13:54:43 +0530

    Add test cases for `get_data_dir` & `get_data_file`

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -1,24 +1,25 @@
 import pytest
-from rice_yield.utils.paths import get_data_path, RAW_DIR
+from pathlib import Path
+from rice_yield.utils.paths import get_data_dir, get_data_file
 
 
-def test_get_data_path_raw() -> None:
-    path = get_data_path("raw", "precipitation.csv")
-    assert str(path).endswith("data\\raw\\precipitation.csv")
-    assert path.parent == RAW_DIR
+def test_get_data_file_valid() -> None:
+    path = get_data_file(get_data_dir("raw"), "example.csv")
+    assert isinstance(path, Path)
+    assert path.parts[-3:] == ("data", "raw", "example.csv")
 
 
-# def test_get_data_path_processed():
-#     path = get_data_path("processed", "cleaned_precipitation.csv")
-#     assert str(path).endswith("data/processed/cleaned_precipitaion.csv")
-#     assert path.parent == PROCESSED_DIR
-
-# def test_get_data_path_final():
-#     path = get_data_path("final", "rice_yield.csv")
-#     assert str(path).endswith("data/final/rice_yield.csv")
-#     assert path.parent == FINAL_DIR
+def test_get_data_dir_valid() -> None:
+    path = get_data_dir("cleaned")
+    assert isinstance(path, Path)
+    assert path.parts[-2:] == ("data", "cleaned")
 
 
-def test_get_data_path_invalid_subfolder() -> None:
-    with pytest.raises(ValueError, match="Invalid subfolder 'invalid'"):
-        get_data_path("invalid", "file.csv")
+def test_invalid_subfolder_path() -> None:
+    with pytest.raises(ValueError):
+        get_data_file(get_data_dir("invalid"), "file.csv")
+
+
+def test_invalid_subfolder_dir() -> None:
+    with pytest.raises(ValueError):
+        get_data_dir("invalid")
