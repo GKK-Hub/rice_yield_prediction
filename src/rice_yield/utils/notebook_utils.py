e5cdd11c74e42412ca7b4e8af79aa47787229870
Gowtham Kumar K <gowthamkumar.kg@gmail.com>
2025-09-26 20:28:32 +0530

    Removed plot functions

--- src/rice_yield/utils/notebook_utils.py
+++ src/rice_yield/utils/notebook_utils.py
@@ -5,45 +5,16 @@ Helper functions used in notebooks for data exploration and visualization.
 """
 
 import pandas as pd
-import numpy as np
 from pathlib import Path
 from IPython.display import display, HTML
 import matplotlib.pyplot as plt
 import seaborn as sns
 from shiny.express import ui, render, input
 from htmltools import TagList, tags, Tag
-import matplotlib.colors as mcolors
 
 from sklearn.pipeline import Pipeline
 import joblib
 import json
-
-
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Rename columns in a DataFrame for brevity or clarity.
-
-    Args:
-        df (pd.DataFrame): Input DataFrame.
-        rename_dict (dict): Dictionary mapping old column names to new ones.
-
-    Returns:
-        pd.DataFrame: DataFrame with renamed columns.
-    """
-    rename_cols = {
-        'rice_area_1000_ha': 'area',
-        'rice_production_1000_tons': 'production',
-        'rice_yield_kg_per_ha': 'yield',
-        'average_actual_evapotranspiration': 'act_etranspiration',
-        'average_potential_evapotranspiration': 'pot_etranspiration',
-        'rice_irrigated_area_1000_ha': 'irrigated_area',
-        'average_precipitation': 'precipitation',
-        'average_water_deficit': 'water_deficit',
-        'average_maximum_temperature': 'max_temperature',
-        'average_minimum_temperature': 'min_temperature',
-        'average_rainfall': 'rainfall'
-    }
-    return df.rename(columns=rename_cols)
 
 
 def render_print(text: str) -> ui.HTML:
@@ -95,126 +66,6 @@ def render_table(df: pd.DataFrame, max_height: str = "600px") -> None:
     </div>
     """
     display(HTML(scrollable))
-
-
-def show_distribution(df: pd.DataFrame) -> tuple[Tag, render.plot, render.ui]:
-    # Define variables and remarks
-    dist_variables = [
-                    'dist_name',
-                    'act_etranspiration',
-                    'pot_etranspiration',
-                    'area',
-                    'production',
-                    'yield',
-                    'irrigated_area',
-                    'max_temperature',
-                    'min_temperature',
-                    'precipitation',
-                    'water_deficit',
-                    'rainfall'
-    ]
-    dist_remarks = {"dist_name": (
-        "Most of the districts have 26 years' data, except Nagapattinam."),
-        "act_etranspiration": (
-        "Almost normally distributed with no outliers or skewness."),
-        "area": (
-        "The data is approximately normal with right skew."),
-        "production": (
-        "The data is approximately normal with right skew."),
-        "yield": (
-        "The `distribution is approximately normal with slight left skew."),
-        "irrigated_area": (
-        "The data is approximately normal with right skew."),
-        "max_temperature": (
-            "Distribution is mostly symmetric with left skew due to outliers."
-            ),
-        "min_temperature": "Almost same as max_temperature",
-        "precipitation": (
-        "The distribution is approximately normal with slight right skew."),
-        "water_deficit": (
-        "Almost normally distributed with no outliers or skewness."),
-        "rainfall": "Almost normally distributed with outliers.",
-        "pot_etranspiration": (
-            "Distribution is approximately normal with slight left skew."
-            )}
-    # Create UI elements
-    user_input = ui.input_select("dist_var",
-                                 "Choose Variable",
-                                 choices=dist_variables
-                                 )
-
-    # Plot renderer
-    @render.plot(width=900, height=400)  # type: ignore
-    def dist_plot() -> None:
-        col = input.dist_var()
-        if col != 'dist_name':
-            fig, ax = plt.subplots(1, 2)
-            plt.style.use('fivethirtyeight')
-            # Axes 1: original distribution
-            sns.distplot(df[col], ax=ax[0])
-            ax[0].set_xlabel(col, fontsize=11)
-            ax[0].set_ylabel("Density", fontsize=11)
-            # Axes 2: box plot of the original distribution
-            sns.boxplot(df[col], ax=ax[1])
-            ax[1].set_ylabel(col, fontsize=11)
-        else:
-            plt.style.use('fivethirtyeight')
-            fig, ax = plt.subplots(1)
-            df[col].value_counts().plot(kind='bar')
-            ax.set_ylim(0, 27)
-            ax.set_yticks(range(1, 27, 5))
-            ax.set_xlabel('')  # Hide the x-axis label
-            ax.set_xticklabels(ax.get_xticklabels(), fontsize=11)
-            ax.set_yticklabels(ax.get_yticklabels(), fontsize=11)
-            ax.xaxis.grid(False)
-        return
-
-    # Remarks renderer
-    @render.ui  # type: ignore
-    def dist_remarks_ui() -> TagList:
-        col = input.dist_var()
-        return TagList(
-            tags.p("Remarks for ",
-                   tags.code(col),
-                   ": ",
-                   tags.strong(dist_remarks.get(col)))
-        )
-    return user_input, dist_plot, dist_remarks_ui
-
-
-def show_correlation_heatmap(corr_df: pd.DataFrame) -> render.plot:
-    @render.plot(width=900, height=700)  # type: ignore
-    def heatmap_plot() -> None:
-
-        plt.style.use('fivethirtyeight')
-
-        fig, ax = plt.subplots(1)
-        # sky_blue_cmap = sns.light_palette('deepskyblue',
-        # as_cmap=True, reverse=True)
-        colors = colors = ['deepskyblue', 'white', 'deepskyblue']
-        cmap = mcolors.LinearSegmentedColormap.from_list('custom_deepskyblue',
-                                                         colors,
-                                                         N=100)
-        norm = mcolors.CenteredNorm()
-        # Plot the heatmap
-        sns.heatmap(corr_df,
-                    annot=True,
-                    cmap=cmap,
-                    vmin=-1,
-                    vmax=1,
-                    fmt='.1f',
-                    mask=np.triu(np.ones_like(corr_df, dtype=bool)),
-                    norm=norm
-                    )
-        plt.title('Heatmap of Correlation Coefficient Matrix',
-                  fontdict={'fontsize': 12,
-                            'fontweight': 'bold',
-                            'family': 'Arial'})
-        ax.set_xticklabels(ax.get_xticklabels(), fontsize=10)
-        ax.set_yticklabels(ax.get_yticklabels(), fontsize=10)
-        ax.xaxis.grid(False)
-        ax.yaxis.grid(False)
-    return heatmap_plot
 
 
 def correlation_with_target(df: pd.DataFrame) -> tuple[Tag,
