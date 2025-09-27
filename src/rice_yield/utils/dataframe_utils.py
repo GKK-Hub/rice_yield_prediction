0553ffe6ad2e485faeee5587a1126899a0959536
Gowtham Kumar K <gowthamkumar.kg@gmail.com>
2025-09-27 19:27:05 +0530

    Changes to `rename_columns`

--- src/rice_yield/utils/dataframe_utils.py
+++ src/rice_yield/utils/dataframe_utils.py
@@ -17,94 +17,92 @@ pipeline.
 import pandas as pd
 from pathlib import Path
 
-district_names = {'Chengalpattu MGR Kancheepuram': 'Kancheepuram',
-                  'Tiruchirapalli Trichy': 'Tiruchirappalli',
-                  'Periyar(Erode)': 'Erode',
-                  'Dindigul Anna': 'Dindigul',
-                  'Virudhunagar Kamarajar': 'Virudhunagar',
-                  'Sivagangai Pasumpon': 'Sivagangai',
-                  'Chidambanar Toothukudi': 'Thoothukudi',
-                  'North Arcot Vellore': 'Vellore',
-                  'South Arcot Cuddalore': 'Cuddalore',
-                  'Tiruvarur': 'Thiruvarur',
-                  'Thiruppur': 'Tiruppur',
-                  'Thiruvannamalai': 'Tiruvannamalai'
-                  }
-
 
 def read_csv(path: Path) -> pd.DataFrame:
     """
-        Returns a `DataFrame` object
+    Returns a `DataFrame` object
 
-        Args:
-            `path` (`Path`) - Path of the file to be read
-        Returns:
-            `pd.DataFrame`
+    Args:
+        `path` (`Path`) - Path of the file to be read
+    Returns:
+        `pd.DataFrame`
     """
     return pd.read_csv(path)
 
 
 def write_csv(df: pd.DataFrame, path: Path) -> None:
     """
-        Creates a `csv` file on the `path` by converting a `DataFrame` object
+    Creates a `csv` file on the `path` by converting a `DataFrame` object
 
-        Args:
-            `df` (`pd.DataFrame`)
-            `path` (`Path`)
-        Returns:
-            `None`
+    Args:
+        `df` (`pd.DataFrame`)
+        `path` (`Path`)
+    Returns:
+        `None`
     """
     df.to_csv(path, index=False)
 
 
 def drop_location_id(df: pd.DataFrame) -> pd.DataFrame:
     """
-        Drops the location identifiers and returns the `DataFrame` object
+    Drops the location identifiers and returns the `DataFrame` object
 
-        Args:
-            `df` (`pd.DataFrame`)
-        Returns:
-            `pd.DataFrame`
+    Args:
+        `df` (`pd.DataFrame`)
+    Returns:
+        `pd.DataFrame`
     """
     return df.drop(
-                   columns=['Dist Code', 'State Code', 'State Name'],
+                   columns=["Dist Code", "State Code", "State Name"],
                    inplace=False
                   )
 
 
 def standardize_district_names(df: pd.DataFrame) -> pd.DataFrame:
     """
-        Standardizes the old district names to present-day names
+    Standardizes the old district names to present-day names
 
-        Args:
-            `df` (`pd.DataFrame`)
-        Returns:
-            `pd.DataFrame`
+    Args:
+        `df` (`pd.DataFrame`)
+    Returns:
+        `pd.DataFrame`
 
     """
+    district_names = {"Chengalpattu MGR Kancheepuram": "Kancheepuram",
+                      "Tiruchirapalli Trichy": "Tiruchirappalli",
+                      "Periyar(Erode)": "Erode",
+                      "Dindigul Anna": "Dindigul",
+                      "Virudhunagar Kamarajar": "Virudhunagar",
+                      "Sivagangai Pasumpon": "Sivagangai",
+                      "Chidambanar Toothukudi": "Thoothukudi",
+                      "North Arcot Vellore": "Vellore",
+                      "South Arcot Cuddalore": "Cuddalore",
+                      "Tiruvarur": "Thiruvarur",
+                      "Thiruppur": "Tiruppur",
+                      "Thiruvannamalai": "Tiruvannamalai"}
     return df.replace(district_names, inplace=False)
 
 
 def normalize_column_names(column: str) -> str:
     """
-        Removes the spaces in column names
+    Removes the spaces in column names
 
-        Args:
-            `column`: (`str`)
-        Returns:
-            `str`
+    Args:
+        `column`: (`str`)
+    Returns:
+        `str`
     """
-    return column.replace(' ', '_').replace('(', '').replace(')', '').lower()
+    return column.replace(" ", "_").replace("(", "").replace(")", "").lower()
 
 
 def map_columns(df: pd.DataFrame) -> pd.Index:
     """
-        Maps the column names with the function `rename_column`
+    Maps the column names with the function `rename_column`
 
-        Args:
-            `df` (`pd.DataFrame`)
-        Returns:
-            `pd.Index`
+    Args:
+        `df` (`pd.DataFrame`)
+    Returns:
+        `pd.Index`
 
     """
     return df.columns.map(lambda col: normalize_column_names(col))
@@ -112,48 +110,36 @@ def map_columns(df: pd.DataFrame) -> pd.Index:
 
 def annual_average(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
     """
-        Averages values over a period of 12 months and store the average
-        in `column_name` For example, if a dataframe has columns like this:
-        'January Precipitation, ..., December Precipitation, this function will
-        take average over all the months and produce a single value which is
-        stored under `column_name`.
+    Averages values over a period of 12 months and store the average
+    in `column_name` For example, if a dataframe has columns like this:
+    "January Precipitation, ..., December Precipitation, this function will
+    take average over all the months and produce a single value which is
+    stored under `column_name`.
 
-        Args:
-            `df` (`pd.DataFrame`)
-            `column_name` (`str`)
-        Returns:
-            `pd.DataFrame`
+    Args:
+        `df` (`pd.DataFrame`)
+        `column_name` (`str`)
+    Returns:
+        `pd.DataFrame`
     """
-    monthly_cols = df.columns.difference(['year', 'dist_name'])
+    monthly_cols = df.columns.difference(["year", "dist_name"])
     df[column_name] = df[monthly_cols].mean(axis=1).round(2)
-    return df[['year', 'dist_name', column_name]]
+    return df[["year", "dist_name", column_name]]
 
 
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
+def rename_columns(df: pd.DataFrame, rename_dict: dict) -> pd.DataFrame:
     """
     Rename columns in a DataFrame for brevity or clarity.
 
     Args:
-        df (pd.DataFrame): Input DataFrame.
-        rename_dict (dict): Dictionary mapping old column names to new ones.
+        `df` (pd.DataFrame): Input DataFrame
+        `rename_dict` (`dict`): Dictionary mapping old column names to new
+                                ones
 
     Returns:
         pd.DataFrame: DataFrame with renamed columns.
     """
-    rename_cols = {
-        'rice_area_1000_ha': 'area',
-        'rice_production_1000_tons': 'production',
-        'rice_yield_kg_per_ha': 'yield',
-        'average_actual_evapotranspiration': 'act_etranspiration',
-        'average_potential_evapotranspiration': 'pot_etranspiration',
-        'rice_irrigated_area_1000_ha': 'irrigated_area',
-        'average_precipitation': 'precipitation',
-        'average_water_deficit': 'water_deficit',
-        'average_maximum_temperature': 'max_temperature',
-        'average_minimum_temperature': 'min_temperature',
-        'average_rainfall': 'rainfall'
-    }
-    return df.rename(columns=rename_cols)
+    return df.rename(columns=rename_dict)
 
 
 if __name__ == "__main__":
