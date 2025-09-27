7ed51ef96a6744fee177a8948cdc947871917c27
Gowtham Kumar K <gowthamkumar.kg@gmail.com>
2025-09-27 19:27:53 +0530

    Removed unnecessary imports

--- src/rice_yield/utils/notebook_utils.py
+++ src/rice_yield/utils/notebook_utils.py
@@ -4,17 +4,14 @@ notebook_utils.py
 Helper functions used in notebooks for data exploration and visualization.
 """
 
-import pandas as pd
-from pathlib import Path
-from IPython.display import display, HTML
-import matplotlib.pyplot as plt
-import seaborn as sns
-from shiny.express import ui, render, input
-from htmltools import TagList, tags, Tag
-
-from sklearn.pipeline import Pipeline
-import joblib
 import json
+import joblib
+from pathlib import Path
+
+import pandas as pd
+from IPython.display import display, HTML
+from shiny.express import ui
+from sklearn.pipeline import Pipeline
 
 
 def render_print(text: str) -> ui.HTML:
@@ -23,6 +20,17 @@ def render_print(text: str) -> ui.HTML:
 
 
 def render_table(df: pd.DataFrame, max_height: str = "600px") -> None:
+    """
+    Renders the pandas dataframe and highlights the rows on mouse hover.
+    The regular dataframe object when printed doesn't have a scroll option
+    and occupies space
+
+    Args:
+        `df` (`pd.DataFrame`)
+        `max_height` (`str`) - Maximum height of the window
+    Returns:
+        `None`
+    """
     html = df.to_html(index=True, table_id="scrollable-table")
     css = """
     <style>
@@ -68,89 +76,6 @@ def render_table(df: pd.DataFrame, max_height: str = "600px") -> None:
     display(HTML(scrollable))
 
 
-def correlation_with_target(df: pd.DataFrame) -> tuple[Tag,
-                                                       render.plot,
-                                                       render.ui]:
-    corr_variables = [
-                    'x',
-                    'act_etranspiration',
-                    'pot_etranspiration',
-                    'area',
-                    'production',
-                    'irrigated_area',
-                    'max_temperature',
-                    'min_temperature',
-                    'precipitation',
-                    'water_deficit',
-                    'rainfall'
-    ]
-    corr_remarks = {"dist_name": (
-        "Most of the districts have 26 years' data, except Nagapattinam."),
-        "act_etranspiration": (
-        "Almost normally distributed with no outliers or skewness."),
-        "area": (
-        "The data is approximately normal with right skew."),
-        "production": (
-        "The data is approximately normal with right skew."),
-        "yield": (
-        "The `distribution is approximately normal with slight left skew."),
-        "irrigated_area": (
-        "The data is approximately normal with right skew."),
-        "max_temperature": (
-            "Distribution is mostly symmetric with left skew due to outliers."
-            ),
-        "min_temperature": "Almost same as max_temperature",
-        "precipitation": (
-        "The distribution is approximately normal with slight right skew."),
-        "water_deficit": (
-        "Almost normally distributed with no outliers or skewness."),
-        "rainfall": "Almost normally distributed with outliers.",
-        "pot_etranspiration": (
-            "Distribution is approximately normal with slight left skew."
-            )}
-    user_input = ui.input_select("corr_var",
-                                 "Choose Variable",
-                                 choices=corr_variables)
-
-    @render.plot(width=900, height=400)  # type: ignore
-    def corr_plot() -> None:
-        col = input.corr_var()
-
-        corr = df['yield'].corr(df[col])
-        # print(f'Correlation: {corr:.2f}')
-
-        # 2. Scatter plot with regression line
-        plt.style.use('fivethirtyeight')
-
-        fig, ax = plt.subplots(1)
-        sns.regplot(x=col, y='yield', data=df, ax=ax)
-        plt.title(f'Scatter Plot (Correlation: {corr:.2f})',
-                  fontdict={'fontsize': 12,
-                            'fontweight': 'bold',
-                            'family': 'Arial'})
-        ax.set_xlabel(ax.get_xlabel(), fontsize=11)
-        ax.set_ylabel(ax.get_ylabel(), fontsize=11)
-        ax.set_xticklabels(ax.get_xticklabels(), fontsize=10)
-        ax.set_yticklabels(ax.get_yticklabels(), fontsize=10)
-
-    @render.ui  # type: ignore
-    def corr_remarks_ui() -> TagList:
-        col = input.corr_var()
-        return TagList(tags.p("Remarks for ", tags.code(col),
-                              ": ",
-                              tags.strong(corr_remarks.get(col))))
-    return user_input, corr_plot, corr_remarks_ui
-
-
-# def get_base_path() -> Path:
-#     """
-#     Return the base path where validation curve plots are stored.
-#     Adjust this if needed to read from environment or config.
-#     """
-#     return Path("outputs/validation_curves")
-# *** Insead of this, we'll call get_validation_curve_dir from paths.py ***
-
-
 def get_model_names(base_path: Path) -> list[str]:
     """
     List all available model names by scanning the base directory.
@@ -164,20 +89,6 @@ def get_model_names(base_path: Path) -> list[str]:
     if not base_path.exists():
         return []
     return sorted([f.name for f in base_path.iterdir() if f.is_dir()])
-
-
-# def get_model_folder(base_path: Path, model_name: str) -> Path:
-#     """
-#     Return the folder path for the selected model.
-
-#     Args:
-#         base_path (Path): Base directory.
-#         model_name (str): Model name folder.
-
-#     Returns:
-#         Path object to the model’s folder.
-#     """
-#     return base_path / model_name
 
 
 def get_param_names(model_folder: Path) -> list[str]:
@@ -254,6 +165,10 @@ def save_metrics(metrics: dict, save_dir: Path, model_name: str) -> Path:
     """
     save_dir.mkdir(parents=True, exist_ok=True)
     file_path = save_dir / f"{model_name}_metrics.json"
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         json.dump(metrics, f, indent=4)
     return file_path
+
+
+if __name__ == "__main__":
+    print(__doc__)
